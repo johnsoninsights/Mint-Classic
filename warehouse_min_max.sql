2872c508a5faf4e25ea63b618fc040e6e8c70f23
johnsoninsights <jge4991@gmail.com>
2024-12-04 16:09:18 -0500

    warehouse_min_max.sql

--- warehouse_min_max.sql
+++ warehouse_min_max.sql
@@ -1,7 +1,11 @@
-SELECT warehouses.warehouseCode, floor(sum(quantityInStock)/warehousePctCap *100) as 'max_stock', sum(quantityInStock) as 'current_Stock', warehousePctCap, floor(sum(quantityInStock)/warehousePctCap *100) - sum(quantityInStock) as remainingCap
+SELECT warehouses.warehouseCode,  
+      floor(sum(quantityInStock)/warehousePctCap *100) as 'max_stock', 
+      sum(quantityInStock) as 'current_Stock', 
+      warehousePctCap, 
+      floor(sum(quantityInStock)/warehousePctCap *100) - sum(quantityInStock) as remainingCap
 FROM mintclassics.products 
-join mintclassics.warehouses on warehouses.warehouseCode = products.warehouseCode
-group by warehouseCode 
-order by remainingCap desc ;
+    JOIN mintclassics.warehouses on warehouses.warehouseCode = products.warehouseCode
+GROUP BY warehouseCode 
+ORDER BY remainingCap desc ;
 
 /* this query run show the max stock a warehouse could hold , the current amount held , the current capacity of the ware house , and how much space is currently left in that warehouse /*
